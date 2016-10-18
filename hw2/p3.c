8454f529c8df5c2e30806fa2064e27c2910ed736
Alan Kai-Sheng Cham <acham1@linux3.cs.uchicago.edu>
2016-10-17 19:22:30 -0500

    Finished the omp fns, need to format results

--- hw2/p3.c
+++ hw2/p3.c
@@ -25,17 +25,22 @@ typedef struct complex_{
 } Complex;
 
 double mandelbrot_serial(int print);
-double mandelbrot_omp(int is_static, int print);
+double mandelbrot_omp(int is_static, int print, int threads);
 void write_result();
 
 int main(int argc, char **argv){
     double time;
-    time = mandelbrot_omp(1, 1);
-    printf("OMP static version took %f seconds.\n",time);
-    time = mandelbrot_omp(0, 1);
-    printf("OMP dynamic version took %f seconds.\n",time);
-    time = mandelbrot_serial(1);
-    printf("Serial version took %f seconds.\n",time);
+    int numTrials = 1;
+    int max = omp_get_max_threads();
+
+    for (int i = 0; i < numTrials; i++) {
+        time = mandelbrot_omp(1, 1, max);
+        printf("OMP static version took %f seconds.\n",time);
+        time = mandelbrot_omp(0, 1, max);
+        printf("OMP dynamic version took %f seconds.\n",time);
+        time = mandelbrot_serial(1);
+        printf("Serial version took %f seconds.\n",time);
+    }
     return 0;
 }
 
@@ -56,34 +61,56 @@ void write_result(char* name, int* U) {
     fclose(f);
 }
 
-double mandelbrot_omp(int is_static, int print) {
+double mandelbrot_omp(int is_static, int print, int threads) {
     /* Fill in your code here */
     int px = 0, py = 0, iter = 0, *U;
-    double tmp;
+    double tmp = 0, tick, tock;
     Complex c, z;
 
     // Allocate global array to collect data 
     U = malloc(DIM*DIM*sizeof(int));
 
-    double tick = omp_get_wtime();
-    //#pragma omp parallel for firstprivate(c, z, iter, U, py), schedule(static)
-    for (px=0; px<= DIM-1; px++) {
-        for (py=0; py<DIM; py++) {
-            c.real = XMIN + px*(XMAX - XMIN)/(double)DIM;
-            c.imag = YMIN + py*(YMAX - YMIN)/(double)DIM;
-            z.real = 0;
-            z.imag = 0;
-            iter = 0;
-            while (z.real*z.real + z.imag*z.imag < 4 && iter < MAX_ITER){
-                tmp = z.real*z.real - z.imag*z.imag + c.real;
-                z.imag = 2*z.real*z.imag + c.imag;
-                z.real = tmp;
-                iter++;
+    if (is_static) {
+        tick = omp_get_wtime();
+#pragma omp parallel for firstprivate(c, z, iter, U, py, tmp), schedule(static), num_threads(threads)
+        for (px=0; px<= DIM-1; px++) {
+            for (py=0; py<DIM; py++) {
+                c.real = XMIN + px*(XMAX - XMIN)/(double)DIM;
+                c.imag = YMIN + py*(YMAX - YMIN)/(double)DIM;
+                z.real = 0;
+                z.imag = 0;
+                iter = 0;
+                while (z.real*z.real + z.imag*z.imag < 4 && iter < MAX_ITER){
+                    tmp = z.real*z.real - z.imag*z.imag + c.real;
+                    z.imag = 2*z.real*z.imag + c.imag;
+                    z.real = tmp;
+                    iter++;
+                }
+                U[px*DIM + py] = iter;
             }
-            U[px*DIM + py] = iter;
+        }
+    } else {
+        tick = omp_get_wtime();
+#pragma omp parallel for firstprivate(c, z, iter, U, py, tmp), schedule(dynamic), num_threads(threads)
+        for (px=0; px<= DIM-1; px++) {
+            for (py=0; py<DIM; py++) {
+                c.real = XMIN + px*(XMAX - XMIN)/(double)DIM;
+                c.imag = YMIN + py*(YMAX - YMIN)/(double)DIM;
+                z.real = 0;
+                z.imag = 0;
+                iter = 0;
+                while (z.real*z.real + z.imag*z.imag < 4 && iter < MAX_ITER){
+                    tmp = z.real*z.real - z.imag*z.imag + c.real;
+                    z.imag = 2*z.real*z.imag + c.imag;
+                    z.real = tmp;
+                    iter++;
+                }
+                U[px*DIM + py] = iter;
+            }
         }
     }
-    double tock = omp_get_wtime();
+
+    tock = omp_get_wtime();
     #ifdef WRITE_RESULT
     if (print && is_static) {
         write_result("omp_static_results.txt", U);
