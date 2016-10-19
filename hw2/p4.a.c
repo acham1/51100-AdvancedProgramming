df2702cb8651dcc47455fdc2cc682683ad690801
Alan VirtualBox <alanchamis@gmail.com>
2016-10-19 03:37:41 -0500

    skeleton of i/o for p4.a.c

--- hw2/p4.a.c
+++ hw2/p4.a.c
@@ -1,5 +1,5 @@
 /** Alan Cham
-  * HW2 Q3 */
+  * HW2 Q4 */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,14 +7,17 @@
 #include <ctype.h>
 #include <string.h>
 
-#define MAX_LEN 500
+#define MAX_LEN 100
 
-typedef enum command {ADD, DELETE, FIND, PRINT, IGNORE, EXCESS, OTHER} COMMAND;
+typedef enum command {ADD, DELETE, FIND, PRINT, IGNORE, EXCESS, EXIT, OTHER} COMMAND;
 
+char burn_to_end(void);
 void print_heading(void);
+char get_string(char* destination, int maximum);
 COMMAND load_command(char* command_text, char* word, char* definition);
 
 int main(int argc, char* argv[]) {
+    int quit = 0;
     char word[MAX_LEN + 1];
     char definition[MAX_LEN + 1];
     char command_text[MAX_LEN + 1];
@@ -23,39 +26,39 @@ int main(int argc, char* argv[]) {
 
     print_heading();
     start = clock();
-    while (!feof(stdin)) {
+    while (!feof(stdin) && !quit) {
         new_command = load_command(command_text, word, definition);
+        if (new_command == ADD || new_command == FIND || new_command == DELETE) {
+            if (!strlen(word)) {
+                printf("Error: no word detected.\n");
+                continue;
+            }
+        }
+        
         switch (new_command) {
         case ADD:
-            if (!strlen(word)) {
-                printf("Error: no word detected.\n");
-            } else if (definition[0] != '"' || definition[strlen(definition) - 1] != '"') {
+            if (definition[0] != '"' || definition[strlen(definition) - 1] != '"')
                 printf("Error: definition must be enclosed by quotation marks.\n");
-            } else {
+            else
                 printf("Adding word %s with definition %s.\n", word, definition);
-            }
             break;
         case DELETE:
-            if (!strlen(word)) {
-                printf("Error: no word detected.\n");
-            } else {
-                printf("Deleting word %s.\n", word);
-            }
+            printf("Deleting word %s.\n", word);
             break;
         case FIND:
-            if (!strlen(word)) {
-                printf("Error: no word detected.\n");
-            } else {
-                printf("Finding word %s.\n", word);
-            }
+            printf("Finding word %s.\n", word);
             break;
         case PRINT:
             printf("Printing dictionary.\n");
             break;
-        case IGNORE:
-            break;
         case EXCESS:
             printf("Error: excessive input length (limit %d chars).\n", MAX_LEN);
+            break;
+        case EXIT:
+            printf("Good-bye.\n");
+            quit = 1;
+            break;
+        case IGNORE:
             break;
         default:
             printf("Error: %s is not a recognized command.\n", command_text);
@@ -63,81 +66,102 @@ int main(int argc, char* argv[]) {
         }
     }
     end = clock();
-    printf("\nTotal Runtime: %lf\n", (double) (start-end)/CLOCKS_PER_SEC);
+    printf("\n==================================================================\n");
+    printf("\nTotal Runtime: %lf\n\n", (double)(start-end)/CLOCKS_PER_SEC);
     return EXIT_SUCCESS;
 }
 
+char burn_to_end(void) {
+    char c;
+    while (!feof(stdin) && (c = getchar()) != '\n');
+    if (c != '\n') 
+        printf("\n");
+    return c;
+}
+
+// returns character after the last stored character
+char get_string(char* destination, int maximum) {
+    int i = 0;
+    char c;
+
+    while (isspace(c = getchar()));
+    ungetc(c, stdin);
+    while (!isspace(c = getchar()) && c != EOF && i < MAX_LEN) 
+        destination[i++] = c;
+    destination[i++] = '\0';
+    return c;
+}
+
 COMMAND load_command(char* command_text, char* word, char* definition) {
-    int i = 0, j = 0, k = 0;
-    COMMAND new_command = OTHER;
+    COMMAND new_command;
+    int i = 0;
     char c;
 
     printf("%% ");
-    while (isspace(c = fgetc(stdin)) && c != '\n');
-    if (feof(stdin) || c == '\n') {
-        printf("%s", c == '\n' ? "" : "\n");
-        return IGNORE;
-    }
     // get first word
-    while (c != EOF && !isspace(c) && i < MAX_LEN) {
-        command_text[i++] = c;
-        c = fgetc(stdin);
-    }
-    if (i == MAX_LEN && !isspace(c) && c != EOF) {
-        while ((c = fgetc(stdin)) != EOF && c != '\n');
-        printf("%s", c == '\n' ? "" : "\n");
+    c = get_string(command_text, MAX_LEN);
+    ungetc(c, stdin);
+    if (c != '\n' && c != EOF && !isspace(c)) {
+        burn_to_end();
         return EXCESS;
-    } else {
-        command_text[i++] = '\0';
-    }
-    // process first word
-    if (strstr(command_text, "\\") == command_text) {
-        return IGNORE;
-    } else if (!strcmp("add", command_text)) {
+    } else if (!strcmp(command_text, "add")) {
         new_command = ADD;
-    } else if (!strcmp("delete", command_text)) {
-        new_command = DELETE;
-    } else if (!strcmp("find", command_text)) {
+    } else if (!strcmp(command_text, "find")) {
         new_command = FIND;
-    } else if (!strcmp("print", command_text)) {
-        while ((c = fgetc(stdin)) != EOF && c != '\n');
+    } else if (!strcmp(command_text, "delete")) {
+        new_command = DELETE;
+    } else if (strstr(command_text, "//") == command_text) {
+        burn_to_end();
+        return IGNORE;
+    } else if (!strcmp(command_text, "print")) {
+        burn_to_end();
         return PRINT;
+    } else if (!strcmp(command_text, "exit")) {
+        burn_to_end();
+        return EXIT;
+    } else if (!strlen(command_text) && (c == '\n' || c == EOF)) {
+        printf(c == EOF ? "\n" : "");
+        return IGNORE;
+    } else {
+        burn_to_end();
+        return OTHER;
     }
+
     // get second word
-    while (c != '\n' && !feof(stdin) && isspace(c)) c = fgetc(stdin);
-    while (c != '\n' && !feof(stdin) && j < MAX_LEN && !isspace(c)) {
-        word[j++] = c;
-        c = fgetc(stdin);
-    }
-    if (j == MAX_LEN && !isspace(c) && c != EOF) {
-        while ((c = fgetc(stdin)) != EOF && c != '\n');
-        printf("%s", c == '\n' ? "" : "\n");
+    c = get_string(word, MAX_LEN);
+    ungetc(c, stdin);
+    if (c != '\n' && c != EOF && !isspace(c)) {
+        burn_to_end();
         return EXCESS;
-    } else {
-        word[j++] = '\0';
+    } else if (new_command != ADD) {
+        burn_to_end();
+        return new_command;
     }
-    // get third word
-    while (c != '\n' && !feof(stdin) && isspace(c)) c = fgetc(stdin);
-    if (c != '\n' && !feof(stdin)) word[j++] = c;
-    while (c != '\n' && !feof(stdin) && j < MAX_LEN && (c = fgetc(stdin)) != '"') {
-        word[j++] = c;
+
+    // get third word(s)
+    while (isspace(c = getchar()) && c != '\n');
+    if (c != EOF && c != '\n') {
+        definition[i++] = c;
+        while ((c = getchar()) != EOF && c != '\n' && c != '"' && i < MAX_LEN) {
+            definition[i++] = c;
+        }
+        if (c == '"' && i < MAX_LEN) {
+            definition[i++] = c;
+        } else if (c != EOF && c != '\n' && i == MAX_LEN) {
+            burn_to_end();
+            return EXCESS;
+        }
+        definition[i++] = '\0';
+        if (c != EOF) {
+            ungetc(c, stdin);
+            burn_to_end();
+        }
     }
-    if (j == MAX_LEN && !isspace(c) && c != EOF) {
-        while ((c = fgetc(stdin)) != EOF && c != '\n');
-        printf("%s", c == '\n' ? "" : "\n");
-        return EXCESS;
-    } else {
-        if (c != '\n' && !feof(stdin)) word[j++] = c;
-        word[j++] = '\0';
-    }
-    if (c != '\n' && c != EOF) {
-        while ((c = fgetc(stdin)) != EOF && c != '\n');
-        printf("%s", c == '\n' ? "" : "\n");
-    }
-    return new_command;
+    return ADD;
 }
 
 void print_heading(void) {
+    printf("\n==================================================================\n\n");
     printf("Alan Cham\n");
     printf("p4.a: Simple Dictionary via Unbalanced Model 1 Binary Tree\n\n");
     printf("Acceptable case-sensitive commands (maximum line length %d chars): \n", MAX_LEN);
@@ -145,6 +169,8 @@ void print_heading(void) {
     printf("    %% delete word\n");
     printf("    %% find word\n");
     printf("    %% print\n");
-    printf("    %% // <rest of line to ignore>\n\n");
-    printf("Begin here: \n");
+    printf("    %% exit\n");
+    printf("    %% // <rest of line to ignore>\n");
+    printf("\n==================================================================\n");
+    printf("\nBegin here:\n\n");
 }
