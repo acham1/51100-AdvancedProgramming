9690d369885bebbd7cba217cf245c13e49884abc
Alan Kai-Sheng Cham <acham1@linux3.cs.uchicago.edu>
2016-10-21 04:08:42 -0500

    ready for report now

--- hw2/p4c/p4c_maketest.c
+++ hw2/p4c/p4c_maketest.c
@@ -3,14 +3,23 @@
 
 int main(int argc, char* argv[]) {
     int tmp;
-    printf("// This file is to test sequential add  only.\n");
+    printf("// This file is to test random add  only.\n");
     printf("// The output of this file can be saved and redirected to stdin\n"
-        "when running p4a.c or p4b.c");
+        "//when running p4a.c or p4b.c");
 
     int max = atoi(argv[1]);
     for (int i = 0; i < max; i++) {
-      printf("add %05d \"%05d\"\n", i, i);
+      tmp = rand() % max;
+      printf("add %05d \"%05d\"\n", tmp, tmp);
     }
+
+    return 0;
+    // out of reach unless remove above return statement
+    for (int i = 0; i < max; i++) {
+      tmp = rand()%max;
+      printf("find %05d \n", tmp);
+    }
+
     return 0;
 
 }
