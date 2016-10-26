eb5c21246a050bdb129bf21f2478ecb5eecc64b4
acham1 <alanchamis@gmail.com>
2016-10-25 23:27:07 -0500

    Fixed p5.a.c

--- hw2/p5/p5.a.c
+++ hw2/p5/p5.a.c
@@ -1,11 +1,12 @@
 /** Alan Cham
-  * HW 2 #5a */
+  * HW 2 #5a 
+  * Non-recursive BST verification */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-/*
 typedef struct Tree_node {
     struct Tree_node *left, *right;
     int data;
@@ -14,46 +15,71 @@ typedef struct Tree_node {
 int main(void) {
     return 0;
 }
-*/
-
 
 // returns 1 if valid BST, 0 otherwise
 int isBST(Tree_node* tree) {
     const int default_stack_len = 100;
-    int stack_len; // max space in stack
-    int stack_pos; // position of next free index in stack
+    int stack_len = defaut_stack_len; // max space in stack
     Tree_node** stack;
-    Tree_node** new_stack;
     Tree_node* tmp;
+    int min, max;
+    int stack_pos;                    // position of next free index in stack
+    int min, max;
+    int* mins;
+    int* maxs;
 
     stack_pos = 1;
-    stack_len = default_stack_len;
-    stack = malloc(sizeof(Tree_node*) * default_stack_len);
     stack[0] = tree;
+    mins[0] = INT_MIN;
+    maxs[0] = INT_MAX;
+    stack = malloc(sizeof(Tree_node*) * default_stack_len);
+    mins = malloc(sizeof(int) * default_stack_len);
+    maxs = malloc(sizeof(int) * default_stack_len);
 
     while (stack_pos > 0) {
-        if (stack_pos > stack_len-2) {
+        if (stack_pos >= stack_len-2) {
             stack_len *= 2;
-            new_stack = malloc(sizeof(Tree_node*) * stack_len);
-            memcpy(new_stack, stack, stack_pos * sizeof(Tree_node*));
-            free(stack);
-            stack = new_stack;
+            tmp_stack = realloc(sizeof(Tree_node*) * stack_len);
+            tmp_mins = realloc(sizeof(int) * stack_len);
+            tmp_maxs = realloc(sizeof(int) * stack_len);
         }
-        tmp = stack[--stack_pos];
+        --stack_pos;
+        tmp = stack[stack_pos];
+        min = mins[stack_pos];
+        max = maxs[stack_pos];
+        if (temp->data > max || temp->data < min) {
+            free(stack);
+            free(mins);
+            free(maxs);
+            return 0;
+        }
         if (tmp->left != NULL) {
-            if (tmp->left->data >= tmp->data) {
+            if (tmp->data == INT_MIN) {    // no int less than INT_MIN; left child impossible
                 free(stack);
+                free(mins);
+                free(maxs);
                 return 0;
             }
-            stack[stack_pos++] = tmp->left;
+            stack[stack_pos] = tmp->left;  
+            mins[stack_pos] = min;         // cannot violate parent trees
+            maxs[stack_pos] = tmp->data-1; // must be less than current
+            ++stack_pos;
         }
         if (tmp->right != NULL) {
-            if (tmp->right->data <= tmp->data) {
+            if (tmp->data == INT_MAX) {    // no int more than INT_MAX; right child impossible
                 free(stack);
+                free(mins);
+                free(maxs);
                 return 0;
             }
-            stack[stack_pos++] = tmp->right;
+            stack[stack_pos] = tmp->right;
+            mins[stack_pos] = tmp->data+1; // must be greater than current
+            maxs[stack_pos] = max;         // cannot violate parent trees
+            ++stack_pos;
         }
     }
+    free(stack);
+    free(mins);
+    free(maxs);
     return 1;
 }
