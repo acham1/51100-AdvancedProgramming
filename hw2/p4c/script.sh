f298b3f42afafcbe5ae04993bc5ea13dbfa6b2ef
Alan VirtualBox <alanchamis@gmail.com>
2016-10-26 23:23:05 -0500

    ready for generating data

--- hw2/p4c/script.sh
+++ hw2/p4c/script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#./simp
+gcc -std=c99 -pedantic -Wall ../p4a.c ../p4a_dictionaryc ../p4a_tree.c -I../
+gcc -std=c99 -pedantic -Wall ../p4b.c ../p4b_dictionaryc ../p4v_tree.c -I../
+
 for file in ./test_input/*
 do
     ./simp <$file >"$file.output"
