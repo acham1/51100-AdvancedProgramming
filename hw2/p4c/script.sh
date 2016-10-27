efb48658bcc152926ffeb35a0eff7567ae7fdd79
Alan Kai-Sheng Cham <acham1@linux1.cs.uchicago.edu>
2016-10-26 23:43:03 -0500

    made raw data

--- hw2/p4c/script.sh
+++ hw2/p4c/script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-gcc -std=c99 -pedantic -Wall ../p4a.c ../p4a_dictionaryc ../p4a_tree.c -I../
-gcc -std=c99 -pedantic -Wall ../p4b.c ../p4b_dictionaryc ../p4v_tree.c -I../
+gcc -std=c99 -pedantic -Wall ../p4a/p4a.c ../p4a/p4a_dictionary.c ../p4a/p4a_tree.c -I../p4a -o ./simp
+gcc -std=c99 -pedantic -Wall ../p4b/p4b.c ../p4b/p4b_dictionary.c ../p4b/p4b_tree.c -I../p4b -o ./bal
 
 for file in ./test_input/*
 do
-    ./simp <$file >"$file.output"
+    bn=$(basename $file)
+    ./simp <$file >./test_output/simp.$bn.output
+    ./bal <$file >./test_output/bal.$bn.output
 done
