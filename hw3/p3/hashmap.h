8e73e20e69f5c96d8f633ff4cf99f9d0c47ea2f7
Alan VirtualBox <alanchamis@gmail.com>
2016-11-18 15:46:50 -0600

    Added stubs to hw3/p3

---
+++ hw3/p3/hashmap.h
@@ -0,0 +1,45 @@
+/** Alan Cham
+  * MPCS 51100
+  * November 14, 2016
+  * Homework 3, p1 */
+
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+
+#include "linkedlist.h"
+
+#define DEFAULT_LOAD_FACTOR_THRESHOLD 0.7
+#define HMP_GROWTH_FACTOR 2
+#define hmp_max(a, b) ((a) > (b) ? (a) : (b))
+  
+typedef struct {
+    void* key;
+    void* value;
+} Element;
+
+typedef struct Hashmap {
+    Linkedlist** buckets;
+    long numkeys;
+    long numbuckets;
+    long maxoccupancy;
+    long minoccupancy;
+    long numminoccupancy;
+    long nummaxoccupancy;
+    double loadfactorthresh;
+    long (*hashfn)(struct Hashmap*, void*);
+} Hashmap;
+
+Hashmap* hmp_create(void);
+
+// return 1 if fail, 0 if success
+int hmp_resize(Hashmap* hmp, long size, int (*cmp)(void*, void*));
+// return 1 if fail, 0 if success
+int hmp_insert(Hashmap* hmp, void* key, void* value, int (*cmp)(void*, void*));
+// return (key, value) if found, (Null, Null) otherwise
+Element hmp_find(Hashmap* hmp, void* key, int (*cmp)(void*, void*));
+// return (key, value) if removed, (Null, Null) otherwise
+Element hmp_remove(Hashmap* hmp, void* key, int (*cmp)(void*, void*));
+void hmp_deepdestroy(Hashmap* hmp);
+void hmp_shallowdestroy(Hashmap* hmp);
+void hmp_traverse(Hashmap* hmp, int (*cmp)(void* key1, void* key2), void (*dosomething)(void*, void*));
+#endif
