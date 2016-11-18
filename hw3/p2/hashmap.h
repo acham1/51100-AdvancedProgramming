198b1b06918be5782fd36e99536f46bf097eeb25
Alan VirtualBox <alanchamis@gmail.com>
2016-11-17 23:36:16 -0600

    copied hashmap stuff to p2

---
+++ hw3/p2/hashmap.h
@@ -0,0 +1,40 @@
+/** Alan Cham
+  * MPCS 51100
+  * November 17, 2016
+  * Homework 3, p2 */
+
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+
+#include "linkedlist.h"
+
+#define DEFAULT_LOAD_FACTOR_THRESHOLD 0.7
+#define HMP_GROWTH_FACTOR 2
+#define hmp_max(a, b) ((a) > (b) ? (a) : (b))
+  
+typedef struct {
+    void* key;
+    void* value;
+} Element;
+
+typedef struct Hashmap {
+    Linkedlist** buckets;
+    long numkeys;
+    long numbuckets;
+    long maxoccupancy;
+    long minoccupancy;
+    long numminoccupancy;
+    long nummaxoccupancy;
+    double loadfactorthresh;
+    long (*hashfn)(struct Hashmap*, void*);
+} Hashmap;
+
+Hashmap* hmp_create(void);
+int hmp_resize(Hashmap* hmp, long size, int (*cmp)(void*, void*));
+int hmp_insert(Hashmap* hmp, void* key, void* value, int (*cmp)(void*, void*));
+Element hmp_find(Hashmap* hmp, void* key, int (*cmp)(void*, void*));
+Element hmp_remove(Hashmap* hmp, void* key, int (*cmp)(void*, void*));
+void hmp_deepdestroy(Hashmap* hmp);
+void hmp_shallowdestroy(Hashmap* hmp);
+void hmp_traverse(Hashmap* hmp, int (*cmp)(void* key1, void* key2), void (*dosomething)(void*, void*));
+#endif
---
+++ hw3/p2/linkedlist.h
@@ -0,0 +1,26 @@
+/** Alan Cham
+  * MPCS 51100
+  * November 17, 2016
+  * Homework 3, p2 */
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef struct Node {
+    void* key;
+    void* value;
+    struct Node* next;
+} Node;
+
+typedef struct Linkedlist {
+    Node* head;
+    unsigned long numnodes;
+} Linkedlist;
+
+Linkedlist* ll_create(void);
+void ll_shallowdestroy(Linkedlist* ll);
+void ll_deepdestroy(Linkedlist* ll);
+Node* ll_find(Linkedlist* ll, void* key, int (*cmp)(void*, void*));
+Node* ll_remove(Linkedlist* ll, void* key, int (*cmp)(void*, void*));
+int ll_insert(Linkedlist* ll, void* key, void* value, int (*cmp)(void*, void*));
+#endif
